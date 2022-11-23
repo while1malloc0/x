5b65a42ee075be698ebce833a0bb42e7c249e4a8
John Turner <johnturner@me.com>
2022-11-23 04:13:02 +0000

    make rust-analyzer work with shell.nix

--- golink-axum/shell.nix
+++ golink-axum/shell.nix
@@ -1,4 +1,11 @@
 { pkgs ? import <nixpkgs> {} }: 
     pkgs.mkShell rec {
-        buildInputs = with pkgs; [ pkgs.rustc pkgs.cargo pkgs.sqlite ];
+        buildInputs = with pkgs; [ 
+            pkgs.rustc 
+            pkgs.cargo 
+            pkgs.sqlite 
+            pkgs.rust.packages.stable.rustPlatform.rustcSrc
+            pkgs.rust.packages.stable.rustPlatform.rustLibSrc
+        ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     }
