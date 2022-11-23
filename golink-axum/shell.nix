a482e64442b9a48e914499e1f086a211ef35cae4
John Turner <johnturner@me.com>
2022-11-23 04:02:28 +0000

    add sqlite3 to deps

--- golink-axum/shell.nix
+++ golink-axum/shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }: 
     pkgs.mkShell rec {
-        buildInputs = with pkgs; [ pkgs.rustc pkgs.cargo ];
+        buildInputs = with pkgs; [ pkgs.rustc pkgs.cargo pkgs.sqlite ];
     }
