93e942e114e004b37d5a50cbd4d15af4ad87cca3
John Turner <johnturner@me.com>
2022-11-24 01:19:29 +0000

    add example path extractor

--- golink-axum/src/main.rs
+++ golink-axum/src/main.rs
@@ -1,9 +1,9 @@
 use axum::{
     routing::get,
     Router,
-    extract::Extension
+    extract::{Extension, Path}
 };
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{Database};
 use std::net::SocketAddr;
 use std::env;
 
@@ -12,14 +12,11 @@ async fn main() {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
     let db = Database::connect(db_url).await.expect("could not connect to DB");
 
-    // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root))
+        .route("/:shortcode", get(shortcode_redirect_handler))
         .layer(Extension(db));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -27,7 +24,10 @@ async fn main() {
         .unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root(Extension(db): Extension<DatabaseConnection>) -> String {
-    format!("{:?}", db)
+async fn root() -> String {
+    "working".to_string()
+}
+
+async fn shortcode_redirect_handler(Path(shortcode): Path<String>) -> String {
+    shortcode
 }
